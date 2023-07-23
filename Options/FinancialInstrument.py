182c4d6915b4200d544660f08c5ad1518bb8afc9
ItoWindsor <itowindsor@gmail.com>
2023-07-23 16:14:43 +0200

    deleted some files

--- Options/FinancialInstrument.py
+++
@@ -1,6 +0,0 @@
-
-
-class FinancialInstrument:
-
-    def __init__(self, T : float):
-        self.T = T
--- Options/UsualPayoffs.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-PAYOFF_CALL = lambda STOCK_PRICE,STRIKE : np.maximum(STOCK_PRICE - STRIKE,0)
-PAYOFF_PUT = lambda STOCK_PRICE,STRIKE : np.maximum(STRIKE - STOCK_PRICE,0)
