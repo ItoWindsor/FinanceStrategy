minor bug fix and added closed form formula for european put & call

EuropeanOption.py
@@ -9,14 +9,14 @@ import model as mo
 
 class EuropeanOption(op.Option):
 
-    def __init__(self,model : str, payoff : Callable,S0 : float, t : float, T : float, r : float, q : float, sigma : float):
-        super().__init__(payoff = payoff,model = model, t = t, S0 = S0, T = T, r = r, q = q, sigma = sigma)
+    def __init__(self,model : str, payoff : Callable,S0 : float, t : float, T : float, r : float, q : float, sigma : float, name : str = None):
+        super().__init__(payoff = payoff,model = model, t = t, S0 = S0, T = T, r = r, q = q, sigma = sigma, name = name)
 
 
 class EuropeanCallOption(EuropeanOption):
 
     def __init__(self,model : str,S0 : float, K : float, t : float, T : float, r : float, q : float, sigma : float, closed_formula : bool = True):
-        super().__init__(payoff = UsualPayoffs.PAYOFF_CALL,model = model, t = t, S0 = S0, T = T, r = r, q = q, sigma = sigma)
+        super().__init__(payoff = UsualPayoffs.PAYOFF_CALL,model = model, t = t, S0 = S0, T = T, r = r, q = q, sigma = sigma, name = "CALL")
         self.K = K
         self.closed_formula = closed_formula
 
@@ -29,6 +29,25 @@ class EuropeanCallOption(EuropeanOption):
             price = self.S0*np.exp(-self.model.q * (self.T - self.t )) * stat.norm.cdf(d1) - self.K*np.exp(-self.model.r*(self.T - self.t )) * stat.norm.cdf(d2)
             return(price)
 
+    def compute_delta(self) -> float:
+        if type(self.model) == mo.BlackScholes:
+            d1,d2 = self.model.compute_d1_d2(strike = self.K, t = self.t, T = self.T)
+            return stat.norm.cdf(d1)
+
+    def compute_gamma(self) -> float:
+        if type(self.model) == mo.BlackScholes:
+            d1, d2 = self.model.compute_d1_d2(strike=self.K, t=self.t, T=self.T)
+            return stat.norm.pdf(d1)/(self.S0 * self.model.sigma * np.sqrt(self.T - self.t))
+
+    def compute_rho(self) -> float:
+        if type(self.model) == mo.BlackScholes:
+            d1, d2 = self.model.compute_d1_d2(strike=self.K, t=self.t, T=self.T)
+            return self.K * (self.T - self.t) * np.exp(-self.model.r * (self.T - self.t)) * stat.norm.cdf(d2)
+
+    def compute_vega(self) -> float:
+        if type(self.model) == mo.BlackScholes:
+            d1, d2 = self.model.compute_d1_d2(strike=self.K, t=self.t, T=self.T)
+            return self.S0*stat.norm.pdf(d1)*np.sqrt(self.T - self.t)
 
     def plot_payoff(self):
         x_min = np.minimum(self.S0,self.K)/2
@@ -44,7 +63,7 @@ class EuropeanCallOption(EuropeanOption):
 class EuropeanPutOption(EuropeanOption):
 
     def __init__(self,model : str, S0 : float, K : float, t : float, T : float, r : float, q : float, sigma : float, closed_formula : bool = True):
-        super().__init__(payoff = UsualPayoffs.PAYOFF_PUT,model = model, t = t, S0 = S0, T = T, r = r, q = q, sigma = sigma)
+        super().__init__(payoff = UsualPayoffs.PAYOFF_PUT,model = model, t = t, S0 = S0, T = T, r = r, q = q, sigma = sigma, name = "PUT")
         self.K = K
 
     def __str__(self):
@@ -61,7 +80,26 @@ class EuropeanPutOption(EuropeanOption):
         plt.show()
 
     def compute_price(self):
-        if(type(self.model) == mo.BlackScholes):
+        if type(self.model) == mo.BlackScholes:
             d1,d2 = self.model.compute_d1_d2(strike = self.K, t = self.t, T = self.T)
             price =  self.K*np.exp(-self.model.r*(self.T - self.t )) * stat.norm.cdf(-d2) - self.S0*np.exp(-self.model.q * (self.T - self.t )) * stat.norm.cdf(-d1)
-            return(price)
+            return price
+
+    def compute_delta(self) -> float:
+        if type(self.model) == mo.BlackScholes:
+            d1,d2 = self.model.compute_d1_d2(strike = self.K, t = self.t, T = self.T)
+            return - stat.norm.cdf(-d1)
+
+    def compute_gamma(self) -> float:
+        if type(self.model) == mo.BlackScholes:
+            d1, d2 = self.model.compute_d1_d2(strike=self.K, t=self.t, T=self.T)
+            return stat.norm.pdf(d1)/(self.S0 * self.model.sigma * np.sqrt(self.T - self.t))
+    def compute_rho(self) -> float:
+        if type(self.model) == mo.BlackScholes:
+            d1, d2 = self.model.compute_d1_d2(strike=self.K, t=self.t, T=self.T)
+            return -self.K*(self.T - self.t)*np.exp(-self.model.r*(self.T - self.t))*stat.norm.cdf(-d2)
+
+    def compute_vega(self) -> float:
+        if type(self.model) == mo.BlackScholes:
+            d1, d2 = self.model.compute_d1_d2(strike=self.K, t=self.t, T=self.T)
+            return self.S0 * stat.norm.pdf(d1) * np.sqrt(self.T - self.t)

Strategy.py
@@ -5,21 +5,22 @@ import EuropeanOption as euro
 dic_pos = {"long" : -1,
            "short" : 1}
 
-def sum_function(f1,f2, STRIKE_input,pos_f2):
+def sum_payoffs(f1,f2, STRIKE_input,pos_f2):
     return lambda STOCK_PRICE : f1(STOCK_PRICE = STOCK_PRICE) + pos_f2*f2(STOCK_PRICE = STOCK_PRICE, STRIKE = STRIKE_input)*(-1)
 
 class OptionStrat:
-    def __init__(self, products : np.array = np.empty(shape = 0) , position : np.array = np.empty(shape = 0)):
+    def __init__(self, products : np.array = np.empty(shape = 0) , position : np.array = np.empty(shape = 0), strat_name : str = None):
         self.products = products
         self.position = position
+        self.strat_name = strat_name
 
     def final_payoff(self):
         f = lambda STOCK_PRICE : self.products[0].payoff(STOCK_PRICE = STOCK_PRICE, STRIKE = self.products[0].K)
         for i in range(1,len(self.products)):
-            f = sum_function(f1= f,f2 = self.products[i].payoff,STRIKE_input= self.products[i].K, pos_f2 = dic_pos[self.position[i]])
-        return(f)
+            f = sum_payoffs(f1= f,f2 = self.products[i].payoff,STRIKE_input= self.products[i].K, pos_f2 = dic_pos[self.position[i]])
+        return f
 
-    def add_products(self, prod, pos : str):
+    def add_products(self, prod , pos : str):
         self.products = np.append(self.products, prod)
         self.position = np.append(self.position, pos)
 
@@ -48,7 +49,7 @@ class OptionStrat:
         fig, ax = plt.subplots()
         for i in range(len(self.products)):
             ax.plot(S0_arr, price_products[i] + dic_pos[self.position[i]]*self.products[i].payoff(STOCK_PRICE = S0_arr, STRIKE = self.products[i].K)*(-1),
-                    label=f"{self.position[i]} {self.products[i]} | K = {self.products[0].K}", alpha=0.7, linestyle='--')
+                    label=f"{self.position[i]} {self.products[i]} | K = {self.products[i].K}", alpha=0.7, linestyle='--')
 
         ax.axvline(x=self.products[0].S0, color="grey", label="$S_0$", linestyle="--")
         ax.plot(S0_arr, PNL_strat, label="custom strat", alpha=1, color="blue")
@@ -58,25 +59,30 @@ class OptionStrat:
 
         plt.grid(); plt.legend()
         plt.ylabel("P&L"); plt.xlabel("stock price")
-        title_str = "P&L CUSTOM STRAT : "
+
+        if self.strat_name is None:
+            title_str = "P&L custom strat : "
+        else:
+            title_str = f"P&L {self.strat_name} : "
+
         for i in range(len(self.products)):
             title_str += f"{self.products[i].K}"
-            if(i < len(self.products)-1):
+            if i < len(self.products)-1:
                 title_str += "-"
         plt.title(title_str)
         plt.show()
 
-
-
+    def compute_features(self, features = ('price',)):
+        pass
 
 class CallSpread(OptionStrat):
 
     def __init__(self,model, K1 : float, K2 : float, S0 : float,t : float, T : float, r : float, q : float, sigma : float, pos :str = "long"):
         call1 = euro.EuropeanCallOption(S0 = S0, K = K1,t = t, T = T, model = model, r = r, q = q, sigma = sigma)
         call2 = euro.EuropeanCallOption(S0 = S0, K = K2,t = t, T = T, model = model, r = r, q = q, sigma = sigma)
-        if(pos == "long"):
+        if pos == "long":
             pos_temp = ["long","short"]
-        elif (pos == "short"):
+        elif pos == "short":
             pos_temp = ["short","long"]
         super().__init__(products = [call1,call2], position = pos_temp)
 
@@ -116,7 +122,6 @@ class CallSpread(OptionStrat):
         PNL_call1 = price_call1 + dic_pos[self.position[0]]*self.products[0].payoff(STOCK_PRICE = S0_arr,STRIKE = self.products[0].K )*(-1)
         PNL_call2 = price_call2 + dic_pos[self.position[1]]*self.products[1].payoff(STOCK_PRICE=S0_arr, STRIKE=self.products[1].K)*(-1)
         PNL_strat = price_strat + final_payoff(STOCK_PRICE=S0_arr, STRIKE1=self.products[0].K, STRIKE2=self.products[1].K)
-        indx_positiv_pnl = np.where(PNL_strat >= 0)[0]
 
         fig, ax = plt.subplots()
         ax.plot(S0_arr, PNL_call1 ,label=f"long CALL | K = {self.products[0].K}", alpha = 0.7, linestyle = '--')